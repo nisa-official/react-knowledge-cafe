aeeda0744fe8fbc7540cf2be73bdc1d47073a399
nisa722023 <younusali1794khan@gmail.com>
2023-09-11 19:31:52 +0600

    my first commit in react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-red-300'>knowledge Cafe</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
