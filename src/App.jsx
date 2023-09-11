5d303679e5dc39d6e204645003cc73f24eb07a84
nisa722023 <younusali1794khan@gmail.com>
2023-09-11 20:20:57 +0600

    commit in blog data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +10,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-6xl bg-red-300'>knowledge Cafe</h1>
+     <Header></Header>
+     <Blogs></Blogs>
       
      
     </>
